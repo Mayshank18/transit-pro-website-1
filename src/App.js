3a5d737ea13e888a1b6ea312ec1b7f1f50296543
rustzz-27 <reshuag19@gmail.com>
2021-11-02 00:49:25 +0700

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignupPage from "./Components/SignupPage";
+import LoginPage from "./Components/LoginPage";
+import Home from "./Components/Home";
+import { AuthProvider } from "./contexts/Authcontext";
+import OrganizationPage from "./Components/OrganizationPage";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
+  // const Login = (details) => {
+  //   console.log(details);
+  // };
+  // const Signup = (Signupdetails) => {
+  //   console.log(Signupdetails);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <LoginPage />
+            </Route>
+            <Route exact path="/signup">
+              <SignupPage />
+            </Route>
+            <PrivateRoute exact path="/organization">
+              <OrganizationPage />
+            </PrivateRoute>
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </>
   );
 }
 

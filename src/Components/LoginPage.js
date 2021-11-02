a5e874f7eb0e1573510b7225b209905ac367b097
rustzz-27 <reshuag19@gmail.com>
2021-11-02 15:20:11 +0700

    Added loginpage

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import { useAuth } from "../Contexts/AuthContext";
+import { useHistory } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 
 export default function LoginPage() {
-    return (
-        <div>
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
+  //   const submit = (e) => {
+  //     e.preventDefault();
+  //     if (!email || !pwd) {
+  //       alert("Email or Password cannot be blank");
+  //     } else {
+  //       setEmail("");
+  //       setPwd("");
+  //     }
+  //   };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/organization");
+    } catch (err) {
+      setError(err.message);
+    }
+    setLoading(false);
+  }
+
+  return (
+    <div>
+      <h2>LOGIN</h2>
+      <form onSubmit={handleSubmit}>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="someone@organization.com"
+            className="form-control"
+            id="email"
+            ref={emailRef}
+            required
+          />
         </div>
-    )
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            className="form-control"
+            id="password"
+            ref={passwordRef}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-sm btn-success"
+          disabled={loading}
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }

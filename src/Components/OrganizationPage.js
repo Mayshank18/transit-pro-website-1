e3cb6db6e7da6b7561ba777f0af82a9216c5af72
rustzz-27 <reshuag19@gmail.com>
2021-11-02 23:44:36 +0700

    Created rough organization page

--- src/Components/OrganizationPage.js
+++ src/Components/OrganizationPage.js
@@ -1,9 +1,83 @@
-import React from "react";
+import React, {  useRef } from "react";
+//import { useHistory } from "react-router-dom";
+// import { Alert } from "react-bootstrap";
 
 export default function OrganizationPage() {
+    const companyRef = useRef();
+    const phonenumberRef = useRef();
+    const whatsappnumberRef=useRef();
+    const addressRef=useRef();
+
+    // const [error, setError] = useState("");
+    // const [loading, setLoading] = useState(false);
+    //const history = useHistory();
+
   return (
-    <div>
-      <h1>This is organization page</h1>
+    <div className="container">
+      <h2>Organization details</h2>
+      <form>
+        {/* {error && <Alert variant="danger">{error}</Alert>} */}
+        <div className="mb-3">
+          <label htmlFor="company" className="form-label">
+            Company Name
+          </label>
+          <input
+            type="text"
+            placeholder="Company Name"
+            className="form-control"
+            id="company"
+            ref={companyRef}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="address" className="form-label">
+            Address
+          </label>
+          <input
+            type="text"
+            placeholder="Address"
+            className="form-control"
+            id="address"
+            ref={addressRef}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phonenumber" className="form-label">
+            Phone Number
+          </label>
+          <input
+            type="number"
+            placeholder="Phone number"
+            className="form-control"
+            id="phonenumber"
+            ref={phonenumberRef}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="whatsappnumber" className="form-label">
+            Whatsapp Number
+          </label>
+          <input
+            type="number"
+            placeholder="Whatsapp Number"
+            className="form-control"
+            id="whatsappnumber"
+            ref={whatsappnumberRef}
+            required
+          />
+        </div>
+        
+        <button
+          type="submit"
+          className="btn btn-sm btn-success"
+        //   disabled={loading}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }

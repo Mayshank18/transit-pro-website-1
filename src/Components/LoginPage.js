73e04919281815c609bc9120aa6fd60566f8a5a9
rustzz-27 <reshuag19@gmail.com>
2021-11-15 23:07:06 +0530

    added css

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -3,9 +3,9 @@ import { useAuth } from "../Contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 import { Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import Header from "./Header";
 import global from "./global";
-
+import "../Styling/loginpage.css"
 export default function LoginPage() {
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -14,78 +14,78 @@ export default function LoginPage() {
   //const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-
   async function handleSubmit(e) {
     e.preventDefault();
-    global.globalEmail=emailRef.current.value;
+    global.globalEmail = emailRef.current.value;
     let cancel = false;
     try {
-      
       setError("");
       // setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value).then(()=>{
-        if (cancel) return;
-      //setLoading(false);
-      })
+      await login(emailRef.current.value, passwordRef.current.value).then(
+        () => {
+          if (cancel) return;
+          //setLoading(false);
+        }
+      );
 
-        console.log(global.signupState+" "+global.globalEmail);
-      if(!global.signupState)
-      history.push("/organization");
-      else
-      history.push("/profile");
+      console.log(global.signupState + " " + global.globalEmail);
+      if (!global.signupState) history.push("/organization");
+      else history.push("/profile");
     } catch (err) {
       setError(err.message);
-     // setLoading(false);
+      // setLoading(false);
     }
-    cancel=true;
+    cancel = true;
     return cancel;
-   
   }
 
   return (
-    <div className="container">
-      <h2>LOGIN</h2>
-      <form onSubmit={handleSubmit}>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            placeholder="someone@organization.com"
-            className="form-control"
-            id="email"
-            ref={emailRef}
-            required
-          />
+    <div>
+      <Header/>
+      <div className="container form">
+        <h2>LOGIN</h2>
+        <form onSubmit={handleSubmit}>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="someone@organization.com"
+              className="form-control"
+              id="email"
+              ref={emailRef}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="form-control"
+              id="password"
+              ref={passwordRef}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-sm btn-success"
+            // disabled={loading}
+          >
+            Login
+          </button>
+        </form>
+        <div>
+          <Link to="/forgot-password">Forgot Password</Link>
         </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            placeholder="password"
-            className="form-control"
-            id="password"
-            ref={passwordRef}
-            required
-          />
+        <div>
+          <Link to="/signup">Need an account</Link>
         </div>
-        <button
-          type="submit"
-          className="btn btn-sm btn-success"
-          // disabled={loading}
-        >
-          Login
-        </button>
-      </form>
-      <div>
-        <Link to="/forgot-password">Forgot Password</Link>
-      </div>
-      <div>
-        <Link to="/signup">Need an account</Link>
       </div>
     </div>
   );

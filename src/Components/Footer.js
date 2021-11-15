c2446bc16a043151a05944b61b6d38f191cbc538
rustzz-27 <reshuag19@gmail.com>
2021-11-15 23:01:36 +0530

    made footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import "../Styling/footer.css"
-
+import React from "react";
+import "../Styling/footer.css";
+import whatsapp from "../Photos/whatsapp-logo.png"
 export default function Footer() {
-    return (
-        <div className="bottomnav"> 
-               <p>Footer</p>
-        </div>
-    )
+  return (
+    <div className="bottomnav">
+      <div className="footer-text">
+        <p>Call us at : 9334399539, 9334899539 OR </p>
+        {/* <img src={whatsapp} alt="" className="whatsapp-logo" /> */}
+      </div>
+    </div>
+  );
 }

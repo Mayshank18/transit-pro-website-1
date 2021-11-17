49834918857d26bd09ab5b330d472c506c5075bd
percius47 <33738242+percius47@users.noreply.github.com>
2021-11-18 02:06:25 +0530

    fix: deploy issue

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../Styling/header.css"
-import logo from '../Images/homepage.png'
+import logo from '../images/homepage.png'
 export default function Header() {
     return (
         <div className="topnav"> 

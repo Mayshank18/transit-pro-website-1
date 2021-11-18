27aed7491dea779f238c9c91ddee62affaaa3b7f
percius47 <33738242+percius47@users.noreply.github.com>
2021-11-18 17:50:45 +0530

    feat:icon add

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,23 +2,39 @@ import React from 'react'
 import Header from './Header'
 import Navbar from './Navbar'
 import "./LandingPage.css"
+import { NavLink } from 'react-router-dom'
+import Footer from './Footer'
+import truck from "../images/truck.png"
 function LandingPage() {
     return (
 
-     < div >
+        <div style={{backgroundColor:"#E5E5E5"}}>
      <Navbar/>
 
-     <div id="row">
-         <div className="column ">
-            mini profile
+     <div id="row" style={{ height:"100vh"}}>
+         <div className="column  col-left">
+         <div className="miniProfile">
+             <h2>Company pvt. Ltd.</h2>
+            <NavLink exact to="/profile" className="prof-link">View Profile</NavLink>
+           <div className="member">
+           <h5>Membership: Platinum</h5>
+           </div>
          </div>
-         <div className="column ">
-             feed
+         
          </div>
-         <div className="column ">
+         {/* middle Column */}
+         <div className="column col-mid">
+            <div className="reqBox">
+                <img src={truck} alt="icon" />
+            </div>
+         </div>
+
+         {/* Right col */}
+         <div className="column  ">
             my utilities
          </div>
      </div>
+     <Footer/>
      </div>
     )
 }

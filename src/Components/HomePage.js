60d8e98c6f0dc6b9878bdb9ae07c8bed3eae391d
percius47 <33738242+percius47@users.noreply.github.com>
2021-12-09 17:22:22 +0530

    routine

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import Footer from "./Footer"
 import "../App.css"
 import "./HomePage.css"
-import Hero from "../images/white.png"
+import Hero from "../images/hero.png"
 import truck from "../images/truck.png"
 import { Helmet } from "react-helmet";
 export default function HomePage() {
@@ -16,7 +16,7 @@ export default function HomePage() {
       <Header/>  
       <div className="divide">
       <div className='Heroleft'>
-        <h1>Businesses grow with</h1>
+        <h1 style={{fontSize: "2rem"}}>Businesses grow with</h1>
     <img src={Hero} alt="Transit Pro" className="banner" />
     <p>This is a platform where your Logistic Companies finds their Future Businesses. </p>
         <Homebutton/>
